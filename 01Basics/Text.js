2cbe542b13447d3f48bb85631b480eb4554b3ff5
Shibaprasad-1234 <103254458+Shibaprasad-1234@users.noreply.github.com>
2023-11-28 07:26:23 +0000

    java-script basic done

---
+++ 01Basics/Text.js
@@ -0,0 +1,11 @@
+console.log("Hello world")
+/* 
+
+1.Java-script is a high level scripting language used to perform the task on front-end as well as back-end.
+2.Java-script use both client side as well as server side. 
+3.Java-script is most used programming language for web development.
+4.In java-script we use "Es6" version is the latest one, developed in 2015.
+   *Es6=Echma script 6 (Vanilla java-script)
+java-script is basically used for designing the logic part of our web pages.
+
+ */
